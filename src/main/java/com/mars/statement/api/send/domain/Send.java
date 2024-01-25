18f6d22d82a84bb132f25c140c332cf05a334817
nnnnnnniam1 <nnnnnnniam1@gmail.com>
2024-01-25 14:56:29 +0900

    전달 - 메세지 전송 시간 추가

--- src/main/java/com/mars/statement/api/send/domain/Send.java
+++ src/main/java/com/mars/statement/api/send/domain/Send.java
@@ -3,11 +3,9 @@ package com.mars.statement.api.send.domain;
 import com.mars.statement.api.chapter.domain.Chapter;
 import com.mars.statement.api.chapter.domain.ChapterMember;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.sql.Timestamp;
 
@@ -16,17 +14,29 @@ import java.sql.Timestamp;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-@Table(name = "tbl_send_message")
+@Setter
+@Table(name = "tbl_send_message",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "unique_send_combination",
+                        columnNames = {"chapter_id", "from_id", "to_id"}
+                )
+        }
+)
+@EntityListeners(AuditingEntityListener.class)
 public class Send {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @ManyToOne
     @JoinColumn(name = "chapter_id", nullable = false)
     private Chapter chapter;
+
     @ManyToOne
     @JoinColumn(name = "from_id", nullable = false)
     private ChapterMember from;
+
     @ManyToOne
     @JoinColumn(name = "to_id", nullable = false)
     private ChapterMember to;
@@ -37,8 +47,10 @@ public class Send {
     @Column(name = "reg_dt")
     @CreatedDate
     private Timestamp reg_dt;
+
     @Column(name = "bookmark_yn")
-    private Boolean bookmark = false;
+    private Boolean bookmark;
+
     @Column(name = "location", length = 100)
     private String location;
 }
