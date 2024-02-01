d6e0679a26ac6495682095e0785afbef37c5237c
2Tory <dldudgus0604@gmail.com>
2024-02-01 15:22:59 +0900

    feat : Add Swagger Configuration

---
+++ src/main/java/com/mars/statement/global/dto/SwaggerExampleValue.java
@@ -0,0 +1,6 @@
+package com.mars.statement.global.dto;
+
+public class SwaggerExampleValue {
+    public static final String LOGIN_SUCCESS_RESPONSE = "{\"status\":200,\"message\":\"로그인 성공\",\"data\":{\"id\":1,\"accessToken\":\"\",\"refreshToken\":\"\"}}";
+    public static final String REISSUE_SUCCESS_RESPONSE = "{\"status\":200,\"message\":\"토큰 재발급 성공\",\"data\":{\"accessToken\":\"\",\"refreshToken\":\"\"}}";
+}
