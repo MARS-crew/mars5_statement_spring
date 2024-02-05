b5e1ad0bc3b8a585ebebb7242c63f4580601cc45
nnnnnnniam1 <101879140+nnnnnnniam1@users.noreply.github.com>
2024-02-05 14:00:48 +0900

    share 디테일 조회 dto, repository

--- src/main/java/com/mars/statement/api/share/dto/ShareDetailDto.java
+++ src/main/java/com/mars/statement/api/share/dto/ShareDetailDto.java
@@ -1,11 +1,13 @@
 package com.mars.statement.api.share.dto;
 
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.sql.Date;
 import java.util.List;
 
 @Getter
@@ -15,22 +17,24 @@ public class ShareDetailDto {
 
     private Long suggestId;
     private String suggest;
-
-    @Transient
-    private Long chapterTimes; // 회차
-
     private Long chapterId;
     private String summary;
 
-//    @JsonIgnore
-//    private ShareMemberDtailDto shareMemberDtailDto;
-//
-//    @Transient
-//    private List<ShareMemberDtailDto> shareMemberList;
-//
-//    public ShareDetailDto(Long suggestId, String suggest, Long chapterTimes, Long chapterId, List<ShareMemberDtailDto> shareMemberList){
-//        this.suggestId = suggestId; this.suggest = suggest;
-//        this.chapterTimes = chapterTimes; this.chapterId = chapterId;
-//        this.shareMemberList = shareMemberList;
-//    }
+    @JsonIgnore
+    private ShareMemberDetailDto shareMemberDetailDto;
+    @Transient
+    private List<ShareMemberDetailDto> memberDetailList;
+
+    public ShareDetailDto(Long suggestId, String suggest, Long chapterId, String summary, ShareMemberDetailDto shareMemberDetailDto){
+        this.suggestId = suggestId; this.suggest = suggest;
+        this.chapterId = chapterId; this.summary = summary;
+        this.shareMemberDetailDto = shareMemberDetailDto;
+    }
+
+    public ShareDetailDto(Long suggestId, String suggest, Long chapterId, String summary, List<ShareMemberDetailDto> memberDetailList){
+        this.suggestId = suggestId; this.suggest = suggest;
+        this.chapterId = chapterId; this.summary = summary;
+        this.memberDetailList = memberDetailList;
+    }
+
 }
