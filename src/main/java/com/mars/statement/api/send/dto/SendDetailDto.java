d06b60cec7e92eae2ccc89dfe6897a96ff79e61f
nnnnnnniam1 <101879140+nnnnnnniam1@users.noreply.github.com>
2024-02-05 17:57:16 +0900

    send detail 조회 dto, repository

---
+++ src/main/java/com/mars/statement/api/send/dto/SendDetailDto.java
@@ -0,0 +1,40 @@
+package com.mars.statement.api.send.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.mars.statement.api.share.dto.ShareMemberDetailDto;
+import jakarta.persistence.Transient;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SendDetailDto {
+
+    private Long suggestId;
+    private String suggest;
+    private Long chapterId;
+    private String summary;
+
+    @JsonIgnore
+    private SendMemberDetailDto sendMemberDetailDto;
+    @Transient
+    private List<SendMemberDetailDto> memberDetailList;
+
+    public SendDetailDto(Long suggestId, String suggest, Long chapterId, String summary, SendMemberDetailDto sendMemberDetailDto){
+        this.suggestId = suggestId; this.suggest = suggest;
+        this.chapterId = chapterId; this.summary = summary;
+        this.sendMemberDetailDto = sendMemberDetailDto;
+    }
+    public SendDetailDto(Long suggestId, String suggest, Long chapterId, String summary, List<SendMemberDetailDto> memberDetailList){
+        this.suggestId = suggestId; this.suggest = suggest;
+        this.chapterId = chapterId; this.summary = summary;
+        this.memberDetailList = memberDetailList;
+    }
+
+
+
+}
