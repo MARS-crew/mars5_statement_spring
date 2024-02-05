c8998f162088a86fbcd1c100961068492d73c233
nnnnnnniam1 <101879140+nnnnnnniam1@users.noreply.github.com>
2024-02-05 20:59:21 +0900

    like 오류 정리

--- src/main/java/com/mars/statement/api/share/controller/ShareController.java
+++ src/main/java/com/mars/statement/api/share/controller/ShareController.java
@@ -14,6 +14,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -73,13 +74,29 @@ public class ShareController {
     @Operation(summary = "공유 의견 좋아요 기능")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description="공유 의견 좋아요 기능 성공 ",
-                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = LikeRequest.class))})
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = LikeRequest.class))),
+            @ApiResponse(responseCode = "200", description = "공유 의견 좋아요 기능 취소 성공",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = LikeRequest.class))),
+            @ApiResponse(responseCode = "500", description = "챕터 또는 멤버를 찾을 수 없음",
+                    content = @Content(mediaType = "application/json", examples = @ExampleObject(value = "{\"code\":404,\"message\":\"내부 서버 오류.\"}"))),
+            @ApiResponse(responseCode = "404", description = "챕터 또는 멤버를 찾을 수 없음",
+                    content = @Content(mediaType = "application/json", examples = @ExampleObject(value = "{\"code\":404,\"message\":\"챕터 또는 멤버를 찾을 수 없습니다.\"}"))),
     })
     @PostMapping("/detail/{chapterId}")
-    public ResponseEntity<?> updateLike(@PathVariable Long chapterId, @RequestBody LikeRequest request, @Parameter(hidden = true) UserDto userDto) throws NotFoundException {
-        int result = likeService.updateLike(chapterId, request.getShareId(), userDto.getId());
-        return CommonResponse.createResponse(200, "공유 의견 좋아요 기능 성공", request);
+    public ResponseEntity<?> updateLike(@PathVariable Long chapterId, @RequestBody LikeRequest request, @Parameter(hidden = true) UserDto userDto) {
+        try {
+            int result = likeService.updateLike(chapterId, request.getShareId(), userDto.getId());
+
+            if (result == 0) {
+                return CommonResponse.createResponse(200, "공유 의견 좋아요 취소 성공", request);
+            } else if (result == 1) {
+                return CommonResponse.createResponse(200, "공유 의견 좋아요 성공", request);
+            } else {
+                return CommonResponse.createResponseMessage(500, "내부 서버 오류");
+            }
+        } catch (NotFoundException e) {
+            return CommonResponse.createResponseMessage(404, "챕터 또는 멤버를 찾을 수 없습니다");
+        }
+
     }
-
-
 }
