30e4d1677db48dee7a64f7811b5604927e104c5d
nnnnnnniam1 <101879140+nnnnnnniam1@users.noreply.github.com>
2024-02-05 20:37:57 +0900

    opinion 좋아요 기능 repository

--- src/main/java/com/mars/statement/api/share/repository/LikeRepository.java
+++ src/main/java/com/mars/statement/api/share/repository/LikeRepository.java
@@ -3,5 +3,9 @@ package com.mars.statement.api.share.repository;
 import com.mars.statement.api.share.domain.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LikeRepository extends JpaRepository<Like, Long> {
+    Optional<Like> findByShareId(Long shareId);
+
 }
