9c7a58768f66e8bb7f9ad4aaa249bb9df89e5973
nnnnnnniam1 <101879140+nnnnnnniam1@users.noreply.github.com>
2024-02-22 13:47:51 +0900

    공유 회차별 조회 dto 수정

--- src/main/java/com/mars/statement/api/share/dto/OpinionDto.java
+++ src/main/java/com/mars/statement/api/share/dto/OpinionDto.java
@@ -4,11 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.sql.Timestamp;
+
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class OpinionDto {
 
+    private Long chapterId;
+    private Timestamp regDt;
     private String opinion;
     private String location;
 
