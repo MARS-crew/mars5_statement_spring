e671e270627cd72a736f41874a0717685cb4e8b8
nnnnnnniam1 <101879140+nnnnnnniam1@users.noreply.github.com>
2024-02-26 17:04:25 +0900

    send 메세지 저장 수정

--- src/main/java/com/mars/statement/api/chapter/repository/ChapterMemberRepository.java
+++ src/main/java/com/mars/statement/api/chapter/repository/ChapterMemberRepository.java
@@ -14,10 +14,10 @@ public interface ChapterMemberRepository extends JpaRepository<ChapterMember, Lo
     ChapterMember findByChapterIdAndGroupMember_Id(@Param("chapterId") Long chapterId, @Param("memberId") Long memberId);
 
     @Query("SELECT cm FROM ChapterMember cm WHERE cm.chapter.id = :chapterId AND cm.groupMember.user.id = :myId")
-    ChapterMember findChapterMemberByChapterIdAndUserId(@Param("chapterId") Long chapterId, @Param("myId") Long myId);
+    ChapterMember findChapterMemberByChapterIdAndUserId(@Param("chapterId") Long chapterId, Long myId);
 
     @Query("SELECT COUNT(cm) > 0 FROM ChapterMember cm WHERE cm.chapter.id = :chapterId AND cm.id = :memberId")
-    boolean existsByChapterIdAndMemberId(@Param("chapterId") Long chapterId, @Param("memberId") Long memberId);
+    boolean existsByChapterIdAndMemberId(@Param("chapterId") Long chapterId, Long memberId);
 
     List<ChapterMember> findByChapter(Chapter chapter);
 
