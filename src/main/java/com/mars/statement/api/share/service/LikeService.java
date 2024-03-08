f11ea8e65fb8ae7256cbcd415c93ca5bb03b4158
2Tory <dldudgus0604@gmail.com>
2024-03-09 01:36:26 +0900

    feat: push notification

--- src/main/java/com/mars/statement/api/share/service/LikeService.java
+++ src/main/java/com/mars/statement/api/share/service/LikeService.java
@@ -4,12 +4,15 @@ import com.mars.statement.api.chapter.domain.Chapter;
 import com.mars.statement.api.chapter.domain.ChapterMember;
 import com.mars.statement.api.chapter.service.ChapterMemberService;
 import com.mars.statement.api.chapter.service.ChapterService;
+import com.mars.statement.api.fcm.service.FcmService;
 import com.mars.statement.api.share.domain.Like;
 import com.mars.statement.api.share.repository.LikeRepository;
 import com.mars.statement.global.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Slf4j
 @Service
@@ -20,12 +23,13 @@ public class LikeService {
     private final ChapterMemberService chapterMemberService;
     private final ChapterService chapterService;
     private final ShareService shareService;
+    private final FcmService fcmService;
 
     public Like getLikeById(Long shareId){
         return likeRepository.findByShareId(shareId).orElse(null);
     }
 
-    public int updateLike(Long chapterId, Long shareId, Long myId) throws NotFoundException {
+    public int updateLike(Long chapterId, Long shareId, Long myId) throws NotFoundException, IOException {
         Chapter chapter = chapterService.getChapterById(chapterId);
         ChapterMember member = chapterMemberService.getChapterMemberByChapterIdAndUserId(chapter.getId(),myId);
         Like like = getLikeById(shareId);
@@ -41,6 +45,9 @@ public class LikeService {
                     .like(true) // 좋아요로 설정
                     .build();
             likeRepository.save(newLike);
+            fcmService.sendMessageTo(shareService.getShareById(shareId).getChapterMember().getGroupMember().getUser().getFcmToken(), "CO:MIT",
+                    member.getUserName()+"님이 좋아요를 눌렀습니다.");
+
             return 1;
         }
 
